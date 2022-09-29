8e5cfdcd90c2d4bed0af6aa32289ccec5be777d3
nicatezimzade <nijatfa@code.edu.az>
2022-09-29 14:52:14 +0300

    Labwork 1 completed!

---
+++ labwork1/labwork1.cpp
@@ -0,0 +1,193 @@
+
+#include <iostream>
+
+//void print_cubed(double x)
+//{
+//	printf("%f\n", pow(x, 3));
+//}
+
+//void Temperatures(double fahrenheit) {
+//	
+//	const double celsius = 5.0 / 9.0 * (fahrenheit - 32.0);
+//	const double kelvin = celsius + 273.16;
+//
+//	printf("%.2lf fahrenheit equals %.2f Celsius and %.2f Kelvin\n\n", fahrenheit, celsius, kelvin);
+//}
+
+int main()
+{
+#pragma region TASK1
+
+	//const int hourInMins = 60;
+
+	//int mins, hours, remainderMins;
+
+	//printf("Enter minute value: ");
+	//scanf_s("%d", &mins);
+
+
+	//while (mins > 0)
+	//{
+	//	hours = mins / hourInMins;
+	//	remainderMins = mins % hourInMins;
+
+	//	printf("%d minutes equals %d hours and %d minutes. \n\n", mins, hours, remainderMins);
+
+	//	printf("Enter minute value (<= 0 to quit): ");
+	//	scanf_s("%d", &mins);
+	//}
+
+	//printf("You exited from the code.\n");
+
+#pragma endregion
+
+#pragma region TASK2
+
+	//int from, to;
+	//printf("Enter an integer: ");
+	//scanf_s("%d", &from);
+	//to = from + 10;
+
+	//while (from<=to) {
+
+	//	printf("%d\n", from);
+	//	from++;
+	//}
+
+#pragma endregion
+
+#pragma region TASK3
+
+	//const int weekInDays = 7;
+
+	//int days, weeks, remainderDays;
+
+	//printf("Enter days value: ");
+	//scanf_s("%d", &days);
+
+
+	//while (days > 0)
+	//{
+	//	weeks = days / weekInDays;
+	//	remainderDays = days % weekInDays;
+
+	//	printf("%d days equals %d weeks and %d days. \n\n", days, weeks, remainderDays);
+
+	//	printf("Enter days value (<= 0 to quit): ");
+	//	scanf_s("%d", &days);
+	//}
+
+	//printf("You exited from the code.\n");
+
+#pragma endregion
+
+#pragma region TASK4
+
+	//const float cmInFoot = 30.48;
+	//const int inchInFoot = 12;
+
+	//float height, feet;
+
+	//printf("Enter a height in centimeters: ");
+	//scanf_s("%f", &height);
+
+	//while (height > 0) {
+	//	feet = height / cmInFoot;
+	//	printf("%.1f cm = %d feet, %.1f inches\n", height, (int)feet, 
+	//		(feet - (int)feet) * inchInFoot);
+
+	//	printf("Enter a height in centimeters (<= 0 to quit): ");
+	//	scanf_s("%f", &height);
+	//}
+	//printf("You exited from the code!\n");
+
+#pragma endregion
+
+#pragma region TASK5
+
+	//int count, sum, maxCount;
+	//sum = 0;
+	//count = 1;
+
+	//printf("How many integers would you like to sum? ");
+	//scanf_s("%d", &maxCount);
+	//while (count <= maxCount)
+	//{
+	//	sum = sum + count;
+	//	count++;
+	//}
+	//printf("sum = %d\n", sum);
+
+#pragma endregion
+
+#pragma region TASK6
+
+	//int count, sum, maxCount;
+	//sum = 0;
+	//count = 1;
+
+	//printf("How many integers would you like to sum? ");
+	//scanf_s("%d", &maxCount);
+	//while (count <= maxCount)
+	//{
+	//	sum = sum + pow(count, 2);
+	//	count++;
+	//}
+	//printf("sum = %d\n", sum);
+
+#pragma endregion
+
+#pragma region TASK7
+
+	//double number;
+
+	//printf("Enter a number: ");
+	//scanf_s("%lf", &number);
+	//print_cubed(number); // print cubed method written in 4-7 lines.
+
+#pragma endregion
+
+#pragma region TASK8
+
+	//int first, second;
+
+	//printf("This program computes moduli.\n"
+	//	"Enter an integer to serve as the second operand: ");
+
+	//scanf_s("%d", &second);
+
+	//printf("Now enter the first operand: ");
+	//scanf_s("%d", &first);
+
+	//while (first > 0) {
+	//	printf("%d %% %d is %d\n", first, second, first % second);
+
+	//	printf("Enter the next number for first operand (<= 0 to quit): ");
+	//	scanf_s("%d", &first);
+	//}
+	//printf("You exited from the code!\n");
+
+#pragma endregion
+
+#pragma region TASK9
+
+	//double fahrenheit;
+
+	//printf("Enter temperature in fahrenheits: ");
+
+	//while (scanf_s("%lf", &fahrenheit) == 1)
+	//{
+	//	Temperatures(fahrenheit); // temperatures method written in 9-15 lines
+
+	//	printf("Enter temperature in fahrenheits: ");
+	//}
+
+	//printf("You exited from the code!");
+
+#pragma endregion
+
+
+
+
+}
+
